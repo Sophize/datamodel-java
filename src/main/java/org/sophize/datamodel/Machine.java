aad8d7366b3703131b4dffcfd90f5713f6b38128
Abhishek Chugh <achugh89@gmail.com>
2020-06-10 17:06:38 +0530

    Datamodel 0.2.3
    https://github.com/Sophize/datamodel-json/releases/0.2.3

--- src/main/java/org/sophize/datamodel/Machine.java
+++ src/main/java/org/sophize/datamodel/Machine.java
@@ -4,16 +4,34 @@ import java.util.*;
 import com.fasterxml.jackson.annotation.*;
 
 public class Machine extends Resource {
+    private Language defaultLanguage;
+    private String defaultLenientStatement;
     private String defaultMaterializeDataset;
+    private String defaultStrictStatement;
     private String description;
     private String[] premiseMachines;
     private String[] premisePropositions;
     private String serverName;
 
+    @JsonProperty("defaultLanguage")
+    public Language getDefaultLanguage() { return defaultLanguage; }
+    @JsonProperty("defaultLanguage")
+    public void setDefaultLanguage(Language value) { this.defaultLanguage = value; }
+
+    @JsonProperty("defaultLenientStatement")
+    public String getDefaultLenientStatement() { return defaultLenientStatement; }
+    @JsonProperty("defaultLenientStatement")
+    public void setDefaultLenientStatement(String value) { this.defaultLenientStatement = value; }
+
     @JsonProperty("defaultMaterializeDataset")
     public String getDefaultMaterializeDataset() { return defaultMaterializeDataset; }
     @JsonProperty("defaultMaterializeDataset")
     public void setDefaultMaterializeDataset(String value) { this.defaultMaterializeDataset = value; }
+
+    @JsonProperty("defaultStrictStatement")
+    public String getDefaultStrictStatement() { return defaultStrictStatement; }
+    @JsonProperty("defaultStrictStatement")
+    public void setDefaultStrictStatement(String value) { this.defaultStrictStatement = value; }
 
     @JsonProperty("description")
     public String getDescription() { return description; }
