b64f1d1ea1964b28348493bdd05aed4babb84196
Abhishek Chugh <achugh89@gmail.com>
2020-07-02 10:23:35 +0530

    Datamodel 0.2.5

--- src/main/java/org/sophize/datamodel/Beliefset.java
+++ src/main/java/org/sophize/datamodel/Beliefset.java
@@ -4,9 +4,15 @@ import java.util.*;
 import com.fasterxml.jackson.annotation.*;
 
 public class Beliefset extends Resource {
+    private String description;
     private String[] subBeliefsetPtrs;
     private String[] unsupportedMachinePtrs;
     private String[] unsupportedPropositionPtrs;
+
+    @JsonProperty("description")
+    public String getDescription() { return description; }
+    @JsonProperty("description")
+    public void setDescription(String value) { this.description = value; }
 
     @JsonProperty("subBeliefsetPtrs")
     public String[] getSubBeliefsetPtrs() { return subBeliefsetPtrs; }
